8b04d187ddc3f20243383c3c768b0b0bef42ccb6
daressss <darapogres@gmail.com>
2026-05-27 13:55:21 +0300

    Update server.js

--- server.js
+++ server.js
@@ -36,8 +36,7 @@ app.use(session({
         secure: true,
         httpOnly: true,
         maxAge: 24 * 60 * 60 * 1000,
-        sameSite: 'none',
-        domain: '.vercel.app'
+        sameSite: 'none'
     }
 }));
 
